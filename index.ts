ad7c592bffe84481072ccbb05a65ae0f17dd0a7f
Fernando74lr <flopezramirez@hotmail.com>
2021-03-02 20:54:18 -0600

    Create REST services in express

--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
 import Server from './classes/server';
+import userRoutes from './routes/user';
 
 const server = new Server();
+
+// Routes
+server.app.use('/user', userRoutes);
+
+
 
 // Raise server
 server.start(() => {

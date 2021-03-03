5209b667b3ec43e8907f6f1e21d9f58313181cb2
Fernando74lr <flopezramirez@hotmail.com>
2021-03-02 21:27:09 -0600

    Data base model: User

---
+++ dist/models/user.model.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.User = void 0;
+const mongoose_1 = require("mongoose");
+const userSchema = new mongoose_1.Schema({
+    nombre: {
+        type: String,
+        required: [true, 'Name is required']
+    },
+    avatar: {
+        type: String,
+        default: 'av-1.png'
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'Email is required']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    }
+});
+exports.User = mongoose_1.model('User', userSchema);

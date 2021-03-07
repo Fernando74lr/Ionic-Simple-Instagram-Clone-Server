1ca4ea9ca80fbc807057fda09ecb26cf3592df67
Fernando74lr <flopezramirez@hotmail.com>
2021-03-06 23:20:47 -0600

    Service to get paginated posts

--- dist/routes/post.js
+++ dist/routes/post.js
@@ -13,6 +13,23 @@ const express_1 = require("express");
 const authentication_1 = require("../middlewares/authentication");
 const post_model_1 = require("../models/post.model");
 const postRoutes = express_1.Router();
+// Get paginated posts
+postRoutes.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    let page = Number(req.query.page) || 1;
+    let skip = page - 1;
+    skip = skip * 10;
+    const posts = yield post_model_1.Post.find()
+        .sort({ _id: -1 })
+        .skip(skip)
+        .limit(10)
+        .populate('user', '-password')
+        .exec();
+    res.json({
+        ok: true,
+        posts
+    });
+}));
+// Create post
 postRoutes.post('/', [authentication_1.verifyToken], (req, res) => {
     const body = req.body;
     body.user = req.user._id;

f51471b6adf82e61c882b68c559abd60e3525647
Fernando74lr <flopezramirez@hotmail.com>
2021-03-12 23:56:58 -0600

    Service to show an image from URL

--- dist/classes/file-system.js
+++ dist/classes/file-system.js
@@ -63,5 +63,9 @@ class FileSystem {
         const pathTemp = path_1.default.resolve(__dirname, '../uploads/', userId, 'temp');
         return fs_1.default.readdirSync(pathTemp) || [];
     }
+    getPhotoUrl(userId, img) {
+        const pathPhoto = path_1.default.resolve(__dirname, '../uploads/', userId, 'posts', img);
+        return pathPhoto;
+    }
 }
 exports.default = FileSystem;
